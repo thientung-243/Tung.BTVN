d89adde4e0acce2975e5c88acb274b11bab36936
boykutepro <55728265+boykutepro@users.noreply.github.com>
2019-10-28 12:11:29 +0700

    btvnn7

--- BTVN_Day07/BTVN_Day07/func.swift
+++ BTVN_Day07/BTVN_Day07/func.swift
@@ -7,3 +7,171 @@
 //
 
 import Foundation
+
+func demNguyenTo()
+{
+    print("Nhap a")
+    let a = Int(readLine()!)!
+    print("Nhap b")
+    let b = Int(readLine()!)!
+    var dem:Int = 0
+    for i in a...b
+    {
+        check(n: i)
+        if check(n: i)
+        {
+            dem += 1
+        }
+    }
+    print(dem)
+}
+func check(n: Int) -> Bool
+{
+    var cnt = 0
+    for i in 1...Int(sqrt(Double(n)))
+    {
+        if n%i == 0
+        {
+            cnt += 1
+        }
+    }
+    if cnt == 1
+    {
+        return true
+    }
+    else{
+        return false;
+    }
+}
+
+func bai2()
+{
+    print("Nhập năm: ")
+    let year = Int(readLine()!)!
+    print("Nhập tháng: ")
+    var month = Int(readLine()!)!
+    
+    switch month {
+    case 1, 3, 5, 7, 8, 10, 12:
+        print("Tháng \(month) có 31 ngày")
+    case 4, 6, 9, 11:
+        print("Thangs \(month) có 30 ngày")
+    case 2:
+        if year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
+        {
+            print("Tháng 2 có 29 ngày")
+        }
+        else{
+            print("Tháng 2 có 28 ngày")
+        }
+    default:
+        print()
+    }
+}
+
+func bai5()
+{
+    print("Nhập mảng: ")
+    let string = readLine()!
+    let arrString = string.components(separatedBy: " ")
+    var arr:[Int] = []
+    for i in 0..<arrString.count
+    {
+        arr.append(Int(arrString[i])!)
+    }
+    print("Nhập X = ")
+    let X = Int(readLine()!)!
+    for i in 0..<arr.count
+    {
+        if arr[i] == timMax(arr: arr)
+        {
+            arr.append(0)
+            for j in (i+1..<arr.count-1).reversed()
+            {
+                arr[j+1] = arr[j]
+            }
+            arr[i+1] = X
+        }
+    }
+}
+func timMax(arr: [Int]) -> Int
+{
+    var m = arr[0]
+    for i in arr{
+        if i > m
+        {
+            m = i
+        }
+    }
+    return m
+}
+
+func bai6()
+{
+    print("Nhập mảng: ")
+    let string = readLine()!
+    let arrString = string.components(separatedBy: " ")
+    var arr:[Int] = []
+    for i in 0..<arrString.count
+    {
+        arr.append(Int(arrString[i])!)
+    }
+    for i in 0..<arr.count
+    {
+        for j in i+1..<arr.count
+        {
+            if arr[i] > arr[j]
+            {
+                let m = arr[i]
+                arr[i] = arr[j]
+                arr[j] = m
+            }
+        }
+    }
+    print("Nhập X = ")
+    let X = Int(readLine()!)!
+    
+    for i in 0..<arr.count-1
+    {
+        if arr[i] <= X && arr[i+1] > X
+        {
+            arr.append(0)
+            for j in (i+1..<arr.count-1).reversed()
+            {
+                arr[j+1] = arr[j]
+            }
+            arr[i+1] = X
+            
+        }
+        
+    }
+    for i in arr
+    {
+        print(i)
+    }
+    
+    
+
+    
+}
+
+//func quickSort(arr: [Int], lo: Int, hi: Int)
+//{
+//    var head = lo
+//    var last = hi
+//    var mid = (lo+hi) / 2
+//    while head < last {
+//        while arr[head] < arr[mid]
+//        {
+//            head += 1
+//        }
+//        while arr[last] > arr[mid]
+//        {
+//            last -= 1
+//        }
+//        swap(arr[last], arr[head])
+//    }
+//    quickSort(arr: arr, lo: mid, hi: hi)
+//    quickSort(arr: arr, lo: lo, hi: mid)
+//
+//}
